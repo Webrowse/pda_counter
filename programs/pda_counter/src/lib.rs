75095350f130fcbf42018c5294c075ebe5f1ad0f
Webrowse <great.adarsh@gmail.com>
2025-07-27 22:47:24 +0530

    bug fixed
    1. space alloted fixed, 2. changed the seed to create a new account, 3. fixed the version problem with js

--- programs/pda_counter/src/lib.rs
+++ programs/pda_counter/src/lib.rs
@@ -7,10 +7,107 @@ pub mod pda_counter {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+        let counter = &mut ctx.accounts.counter; //instance created
+        counter.count = 0; //initialied an account using that instance
+        counter.user = *ctx.accounts.user.key;
+
+        emit!(CounterInit{
+            user: counter.user,
+            count: counter.count,
+        });
+
+        Ok(())
+    }
+
+    pub fn increment(ctx: Context<Increment>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        if counter.user != *ctx.accounts.user.key {
+            return err!(CounterError::InvalidUser);
+        }
+        counter.count += 1;
+
+        emit!(CounterIncrease {
+            user: counter.user,
+            count: counter.count,
+        });
+
+        Ok(())
+    }
+
+    pub fn reset (ctx: Context<Reset>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        if counter.user != *ctx.accounts.user.key {
+            return err!(CounterError::InvalidUser);
+        }
+        counter.count = 0;
+
+        emit!(CounterReset{
+            user: counter.user,
+        });
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(
+        init,
+        payer = user,
+        space = 8 + 32 + 8,
+        seeds = [b"counte", user.key().as_ref()], bump
+    )]
+    pub counter: Account<'info, Counter>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>
+}
+
+#[derive(Accounts)]
+pub struct Increment<'info> {
+    #[account(
+        mut,
+        seeds = [b"counte", user.key().as_ref()], bump
+    )]
+    pub counter: Account<'info, Counter>,
+    pub user: Signer<'info>
+}
+
+#[derive(Accounts)]
+pub struct Reset<'info> {
+    #[account(mut,
+        seeds = [b"counte", user.key().as_ref()], bump,
+        close = user,
+    )]
+    pub counter: Account<'info, Counter>,
+    pub user: Signer<'info>,
+}
+
+#[account]
+pub struct Counter {
+    pub user: Pubkey,
+    pub count: u64,
+}
+
+#[event]
+pub struct CounterInit {
+    pub user: Pubkey,
+    pub count: u64,
+
+}
+#[event]
+pub struct CounterIncrease {
+    pub user: Pubkey,
+    pub count: u64,
+}
+
+#[event]
+pub struct CounterReset {
+    pub user: Pubkey,
+}
+
+#[error_code]
+pub enum CounterError {
+    #[msg("Unauthorised User")]
+    InvalidUser,
+}
--- tests/pda_counter.js
+++ tests/pda_counter.js
@@ -1,13 +1,63 @@
 const anchor = require("@coral-xyz/anchor");
+const assert = require("assert");
 
 describe("pda_counter", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const program = anchor.workspace.pdaCounter;
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  const program = anchor.workspace.pdaCounter;
+
+  let user = provider.wallet.publicKey;
+  let counterPda;
+
+  it("Initialise the counter", async () => {
+    [counterPda] = anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("counte"), user.toBuffer()],
+      program.programId
+    );
+
+    await program.methods.initialize().rpc({
+      accounts: {
+        counter: counterPda,
+        user: user,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      },
+    });
+
+    const counterAccount = await program.account.counter.fetch(counterPda);
+    assert.strictEqual(counterAccount.count.toNumber(), 0);
+    assert.ok(counterAccount.user.equals(user));
+    console.log("Initialised: count = ", counterAccount.count.toNumber());
+  });
+
+  it("Increment the counter ", async () => {
+    await program.methods.increment().rpc({
+      accounts: {
+        counter: counterPda,
+        user,
+      },
+    });
+
+    const counterAccount = await program.account.counter.fetch(counterPda);
+    assert.strictEqual(counterAccount.count.toNumber(), 1);
+    console.log("Incremented: count = ", counterAccount.count.toNumber());
+  });
+
+  it("Reset and close the counter", async () => {
+    await program.methods.reset().rpc({
+      accounts: {
+        counter: counterPda,
+        user,
+      },
+    });
+
+    let didThrow = false;
+    try {
+      await program.account.counter.fetch(counterPda);
+    } catch (_) {
+      didThrow = true;
+    }
+    assert.ok(didThrow);
+    console.log("Counter Reset and Closed");
   });
 });
